b99e3b03eb27e6075eb9b165ecb848b0a458340b
SanjalJain <sanjaljain2019@gmail.com>
2025-05-01 20:33:02 +0530

    fff

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>Hello!</div>
+      <div>this is my react app, implentated using cicp pipeline</div>
+    </>
+  )
+}
+
+export default App
